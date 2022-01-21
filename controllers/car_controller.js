0c6e7270b1ebd59390a66e3969bad49f66e675ab
smiethereens23nas <nicholas23smith@gmail.com>
2022-01-21 11:47:04 -0500

    index page created

--- controllers/car_controller.js
+++ controllers/car_controller.js
@@ -8,7 +8,7 @@ router.get("/", function(req, res) {
 
 router.post('/create', (req, res) => {
     // Start by console logging things out here for the req, then req.body
-    Car.create(req.body, (error, createdCars) => {
+    cars.create(req.body, (error, createdCars) => {
         if(error) console.log(error);
         console.log(createdCars);
 
@@ -17,145 +17,148 @@ router.post('/create', (req, res) => {
     })
 })
 
-// Car.deleteMany({}, (error, deletedCar) => {
-//     if(error) console.log(error);
-//     Car.insertMany(
-//         [
-//             {
-//                 name: "Eric and Troy's Awesome class",
-//                 price: 100,
-//                 image: "https://i.pinimg.com/originals/81/6b/83/816b83a23a6f4b0405bd6699b854a6bd.jpg",
-//                 description: "This is the best class because they named themselves the blue devils!",
-//             },
-//             {
-//                 name: "Some other thing",
-//                 price: 200,
-//                 image: "https://www.rd.com/wp-content/uploads/2021/03/GettyImages-1133605325-scaled-e1617227898456.jpg",
-//                 description: "Another description!",
-//             },
-//             {
-//                 name: "Coders Unite",
-//                 price: 500,
-//                 image: "https://hips.hearstapps.com/countryliving.cdnds.net/17/47/1511194376-cavachon-puppy-christmas.jpg",
-//                 description: "We love coding!",
-//             },
-//         ],
-//           function (error, createdCar) {
-//             if (error) {
-//               return console.log(error);
-//             }
-//             console.log("=== Seed Complete ===");
-//             console.log(createdCar);
-//           }
-//     )
-//     console.log(deletedCar)
-// }
-// )
+Car.deleteMany({}, (error, deletedCar) => {
+    if(error) console.log(error);
+    Car.insertMany(
+        [
+            {
+                car_make: "Tesla",
+                car_model: "Roadster",
+                price: 200000,
+                image: "https://www.teslarati.com/wp-content/uploads/2021/01/tesla-roadster-production-release-candidate-scaled.jpg",
+                description: "A roadster (also spider, spyder) is an open two-seat car with emphasis on sporting appearance or character. Initially an American term for a two-seat car with no weather protection, usage has spread internationally and has evolved to include two-seat convertibles.",
+            },
+            {
+                car_make: "Tesla",
+                car_model: "Cybertruck",
+                price: 40000,
+                image: "https://gmauthority.com/blog/wp-content/uploads/2019/11/Tesla-Cybertruck-Front-Three-Quarter-720x340.jpg",
+                description: "Cybertruck is built with an exterior shell made for ultimate durability and passenger protection. Starting with a nearly impenetrable exoskeleton, every component is designed for superior strength and endurance, from Ultra-Hard 30X Cold-Rolled stainless-steel structural skin to Tesla armor glass.",
+            },
+            {
+                car_make: "Tesla",
+                car_model: "Model S",
+                price: 95000,
+                image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5ANZKvUeNGeOuQQw82HmbK9dSVPFOJNL0CA&usqp=CAU",
+                description: "Model S is built from the ground up as an electric vehicle, with a high-strength architecture and floor-mounted battery pack for incredible occupant protection and low rollover risk. Every Model S includes Tesla's latest active safety features, such as Automatic Emergency Braking, at no extra cost.",
+            },
+        ],
+          function (error, createdCar) {
+            if (error) {
+              return console.log(error);
+            }
+            console.log("=== Seed Complete ===");
+            console.log(createdCar);
+          }
+    )
+    console.log(deletedCar)
+}
+)
 
-// router.get('/', (req, res) => {
+router.get('/', (req, res) => {
     
-//     Car.find({}, (error, foundCars) => {
-//         if(error) return console.log(error);
+    Car.find({}, (error, foundCars) => {
+        if(error) return console.log(error);
 
-//         console.log(foundCars)
-//         context = {
-//             Cars: foundCars
-//         }
-//         res.render('index.ejs', context);
-//     })
-//     /* 
-//     1. the first param of render() is the .ejs file 
-//     that we want to inject data into.
+        console.log(foundCars)
+        context = {
+            Cars: foundCars
+        }
+        res.render('index.ejs', context);
+    })
+    /* 
+    1. the first param of render() is the .ejs file 
+    that we want to inject data into.
     
-//     2. the second param is the data that we want 
-//     to inject into the .ejs file (it must be an object)
-//     */
+    2. the second param is the data that we want 
+    to inject into the .ejs file (it must be an object)
+    */
 
-//     /*	
-//     there will be a variable available inside
-//     the show.ejs file called Car, 
-//     and its value the foundItem
-//    */
+    /*	
+    there will be a variable available inside
+    the show.ejs file called Car, 
+    and its value the foundItem
+   */
 
-// });
+});
 
-// router.post('/', (req, res) => {
-//     // Start by console logging things out here for the req, then req.body
-//     Cars.create(req.body, (error, createdCar) => {
-//         if(error) console.log(error);
-//         console.log(createdCar);
+router.post('/', (req, res) => {
+    // Start by console logging things out here for the req, then req.body
+    cars.create(req.body, (error, createdCar) => {
+        if(error) console.log(error);
+        console.log(createdCar);
 
 
-//         res.redirect("/Cars");
-//     })
-// })
+        res.redirect("/cars");
+    })
+})
 
-// router.get("/create", function(req, res) {
-//     res.render("new.ejs")
-// })
+router.get("/create", function(req, res) {
+    res.render("new.ejs")
+})
 
-// // show route
-// // this route will catch GET requests to /Cars/index/ and respond with a single Car
-// router.get('/:CarId', (req, res) => {
+// show route
+// this route will catch GET requests to /Cars/index/ and respond with a single Car
+router.get('/:CarId', (req, res) => {
     
-//     Cars.findById(req.params.CarId, (error, foundCar) => {
-//         if (error) {
-//             console.log(req.params)
-//             console.log(error);
-//             const context = { error: error };
-//             return res.status(404).render("404", context);
-//         }
-//         /* 
-//         1. the first param of render() is the .ejs file 
-//         that we want to inject data into.
+    cars.findById(req.params.CarId, (error, foundCar) => {
+        if (error) {
+            console.log(req.params)
+            console.log(error);
+            const context = { error: error };
+            return res.status(404).render("404", context);
+        }
+        /* 
+        1. the first param of render() is the .ejs file 
+        that we want to inject data into.
         
-//         2. the second param is the data that we want 
-//         to inject into the .ejs file (it must be an object)
-//         */
+        2. the second param is the data that we want 
+        to inject into the .ejs file (it must be an object)
+        */
 
-//         /*	
-//         there will be a variable available inside
-//         the show.ejs file called Car, 
-//         and its value the foundItem
-//        */
-//         res.render('show.ejs', {Car: foundCar});
-//     });
+        /*	
+        there will be a variable available inside
+        the show.ejs file called Car, 
+        and its value the foundItem
+       */
+        res.render('show.ejs', {car: foundCar});
+    });
     
-// });
+});
 
-// router.delete('/:CarId', (req, res) => {
-//     Cars.findByIdAndDelete(req.params.CarId, (error, deleteCar) => {
-//         if(error) {
-//             console.log(error);
-//             res.send(error);
-//         }
+router.delete('/:CarId', (req, res) => {
+    cars.findByIdAndDelete(req.params.CarId, (error, deleteCar) => {
+        if(error) {
+            console.log(error);
+            res.send(error);
+        }
 
-//         console.log(deleteCar);
-//         res.redirect('/Cars')
-//     })
-// })
+        console.log(deleteCar);
+        res.redirect('/cars')
+    })
+})
 
-// router.get('/:CarId/edit', (req, res) => {
-//     Cars.findById(req.params.CarId, (error, updatedCar) => {
-//         if(error) console.log(error);
+router.get('/:CarId/edit', (req, res) => {
+    cars.findById(req.params.CarId, (error, updatedCar) => {
+        if(error) console.log(error);
 
-//         console.log(updatedCar);
-//         res.render('edit.ejs', { Car: updatedCar})
-//     })
-// })
+        console.log(updatedCar);
+        res.render('edit.ejs', { car: updatedCar})
+    })
+})
 
-// router.put('/:CarId', (req, res) => {
-//     console.log(`The request is ${req}`)
-//     // console.log(`The request's body is ${req.body}`)
+router.put('/:CarId', (req, res) => {
+    console.log(`The request is ${req}`)
+    // console.log(`The request's body is ${req.body}`)
 
-//     Cars.findByIdAndUpdate(req.params.CarId, req.body,(error, updatedCar) => {
-//         if (error) return console.log(error);
+    cars.findByIdAndUpdate(req.params.CarId, req.body,(error, updatedCar) => {
+        if (error) return console.log(error);
 
-//         console.log(updatedCar);
+        console.log(updatedCar);
 
-//         return res.redirect(`/Cars`);
-//     });
-// });
+        return res.redirect(`/cars`);
+    });
+});
 
 module.exports = router;
 
